9f50712ca62a2af7a43cb42f259c229fa7e20b29
Ariadna Frias <ariadna.812@gmail.com>
2023-08-03 12:21:14 +0200

    Add vue component and style

---
+++ src/components/HomePage.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="home">
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Tech stack</li>
+        <li>Projects</li>
+        <ul>
+          <li>Full-stack projects</li>
+          <li>Front-end projects</li>
+        </ul>
+        <li>Contact</li>
+      </ul>
+    </nav>
+    <div class="container">
+      <p>Hello</p>
+      <div></div>
+      <div class="image"></div>
+    </div>
+  </section>
+</template>
+
+<style>
+.home {
+  background-color: rgb(255, 255, 255);
+}
+.image {
+  background-image: url(https://i.gifer.com/JXA0.gif);
+}
+</style>
