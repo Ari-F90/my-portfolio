f412321c23ed35e94f1e98132f83087412b75d29
Ariadna Frias <ariadna.812@gmail.com>
2023-08-04 12:56:20 +0200

    Update index with router

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import { createRouter, createWebHistory } from "vue-router";
+import HomePage from "../components/HomePage.vue";
+import AboutPage from "../components/AboutPage.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "HomePage",
+    component: HomePage,
+  },
+  { path: "/about", name: "AboutPage", component: AboutPage },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
