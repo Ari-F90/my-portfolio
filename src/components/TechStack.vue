a7b3b885f225406cd4207074ec4dac57594b58b8
Ariadna Frias <ariadna.812@gmail.com>
2023-08-04 18:41:09 +0200

    Add tech stack component

---
+++ src/components/TechStack.vue
@@ -0,0 +1,25 @@
+<template>
+  <section id="tech" class="tech">
+    <h1 class="title">My Tech Stack</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi odio harum
+      rem dolorem consequatur quidem accusamus beatae perspiciatis reprehenderit
+      maxime necessitatibus laborum temporibus possimus adipisci libero
+      quibusdam ipsum, neque omnis.
+    </p>
+  </section>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TechStack",
+});
+</script>
+
+<style>
+.tech {
+  background-color: rgb(255, 255, 255);
+}
+</style>
