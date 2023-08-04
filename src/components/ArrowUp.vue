0688fb72ca6595024e10fc372298282700659bb7
Ariadna Frias <ariadna.812@gmail.com>
2023-08-04 18:40:59 +0200

    Add arrow component

---
+++ src/components/ArrowUp.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="arrow-container">
+    <a href="#home"
+      ><img class="arrow" alt="move-up arrow" src="../../public/img/up.png"
+    /></a>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ArrowUp",
+});
+</script>
+
+<style>
+.arrow-container {
+  padding-right: 3rem;
+}
+.arrow {
+  width: 3rem;
+  display: block;
+  position: fixed;
+  bottom: 3rem;
+  right: 91rem;
+}
+</style>
