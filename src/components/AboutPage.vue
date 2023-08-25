0b41add794a1e1ab8fb0c85788b688d5b8390c5b
Ariadna Frias <ariadna.812@gmail.com>
2023-08-25 11:37:34 +0200

    Add skills and hobbies title

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -4,13 +4,14 @@
     <div class="my-desc">
       <p class="first-p">
         After almost 9 years in the markets and financial sector, I came across
-        with the programming world. <br />In 2023, I finished the
+        with the programming world. <br />
+        In 2023, I finished the
         <span class="strong">ISDI Coders</span> (2021 World's best coding
         bootcamp) course
         <span class="strong">"Full-stack web development program".</span>
-        <br />My goal is to continue learning and improving my skills while
-        contributing to meaningful projects that can make a difference, while
-        exploring
+        <br /><br />My goal is to continue learning and improving my skills
+        while contributing to meaningful projects that can make a difference,
+        while exploring
         <span class="strong">new challenges and opportunities</span> in this
         field.
       </p>
@@ -19,37 +20,67 @@
       <div class="skills">
         <span class="skills-title">SOFT SKILLS </span>
         <div class="skills-icon">
-          <img
-            src="../../public/img/teamplayer.png"
-            alt="teamplayer"
-            class="icon"
-          />
-          <img src="../../public/img/time.png" alt="time" class="icon" />
-          <img
-            src="../../public/img/solution.png"
-            alt="solution"
-            class="icon"
-          />
-          <img
-            src="../../public/img/communication.png"
-            alt="communication"
-            class="icon"
-          />
-          <img
-            src="../../public/img/adapt.png"
-            alt="adpatablity"
-            class="icon"
-          />
+          <div class="div-icon">
+            <div class="hidden-skill">Teamwork</div>
+            <img
+              src="../../public/img/teamplayer.png"
+              alt="teamplayer"
+              class="icon"
+            />
+          </div>
+          <div class="div-icon">
+            <div class="hidden-skill">Time management</div>
+            <img src="../../public/img/time.png" alt="time" class="icon" />
+          </div>
+          <div class="div-icon">
+            <div class="hidden-skill">Problem-solving</div>
+            <img
+              src="../../public/img/solution.png"
+              alt="solution"
+              class="icon"
+            />
+          </div>
+          <div class="div-icon">
+            <div class="hidden-skill">Communication</div>
+            <img
+              src="../../public/img/communication.png"
+              alt="communication"
+              class="icon"
+            />
+          </div>
+          <div class="div-icon">
+            <div class="hidden-skill">Adaptability</div>
+            <img
+              src="../../public/img/adapt.png"
+              alt="adaptablity"
+              class="icon"
+            />
+          </div>
         </div>
       </div>
       <div class="hobbies">
         <span class="hobbies-title">HOBBIES</span>
         <div class="hobbies-icon">
-          <img src="../../public/img/coding.png" alt="coding" class="icon" />
-          <img src="../../public/img/music.png" alt="music" class="icon" />
-          <img src="../../public/img/sport.png" alt="sport" class="icon" />
-          <img src="../../public/img/travel.png" alt="travel" class="icon" />
-          <img src="../../public/img/read.png" alt="read" class="icon" />
+          <div class="div-icon">
+            <div class="hidden-skill">Programming</div>
+            <img src="../../public/img/coding.png" alt="coding" class="icon" />
+          </div>
+          <div class="div-icon">
+            <div class="hidden-skill">Music</div>
+            <img src="../../public/img/music.png" alt="music" class="icon" />
+          </div>
+          <div class="div-icon">
+            <div class="hidden-skill">Sport</div>
+            <img src="../../public/img/sport.png" alt="sport" class="icon" />
+          </div>
+          <div class="div-icon">
+            <div class="hidden-skill">Travelling</div>
+            <img src="../../public/img/travel.png" alt="travel" class="icon" />
+          </div>
+          <div class="div-icon">
+            <div class="hidden-skill">Reading</div>
+            <img src="../../public/img/read.png" alt="read" class="icon" />
+          </div>
         </div>
       </div>
     </div>
@@ -112,10 +143,28 @@ export default defineComponent({
   font-family: "Ysabeau Infant", sans-serif;
   font-size: 1.2rem;
   gap: 4rem;
+  justify-self: center;
 }
 .skills-title {
   align-self: center;
-  border-bottom: 2px solid rgb(255, 255, 255);
+}
+.div-icon {
+  position: relative;
+  overflow: hidden;
+  width: 7rem;
+}
+.div-icon > div {
+  display: flex;
+  position: absolute;
+  top: 1rem;
+  left: 0;
+  z-index: -1;
+  margin: 0;
+}
+.hidden-skill {
+  font-size: 1rem;
+  justify-self: center;
+  align-self: center;
 }
 .skills-icon {
   display: flex;
@@ -125,12 +174,14 @@ export default defineComponent({
 .icon {
   width: 4.5rem;
   cursor: pointer;
-  transition: 0.7s;
+  transition: 0.3s;
+}
+.div-icon:hover {
+  transform: scale(1.1);
 }
 .icon:hover {
-  transform: scale(1.3);
-  opacity: 0.5;
-  transition: 0.7s;
+  transition: 0.3s;
+  opacity: 0.07;
 }
 
 .hobbies {
@@ -143,7 +194,6 @@ export default defineComponent({
 }
 .hobbies-title {
   align-self: center;
-  border-bottom: 2px solid rgb(255, 255, 255);
 }
 .hobbies-icon {
   display: flex;
